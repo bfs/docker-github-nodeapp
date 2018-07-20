9344bdf09291c51c9f1e231545129faa7c0faeb6
Boris Shimanovsky <boris@factual.com>
2018-07-19 19:00:48 -0700

    initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # docker-github-nodeapp
 development container for nodeapps 
+
+## Start Example
+
+```bash
+
+docker run --rm -p 8080:8080  --name github-nodeapp -e GITHUB_REPO="https://github.com/uber/kepler.gl.git" -e GITHUB_SSH_KEY="`cat ~/.ssh/github_rsa`"  -e MapboxAccessToken="YOUR_TOKEN_HERE" boritzio/docker-github-nodeapp
+
+```
---
+++ pull_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "$GITHUB_SSH_KEY" > ~/.ssh/github_key && chmod 600 ~/.ssh/github_key
+
+GIT_BRANCH=${GIT_BRANCH:-"master"}
+
+cd /home/app/webapp
+
+if [ -z "$GIT_PATH" ]
+  then
+    git clone $GITHUB_REPO --branch $GIT_BRANCH --single-branch .
+  else
+    git clone -n $GITHUB_REPO --branch $GIT_BRANCH --single-branch .
+    git config core.sparseCheckout true
+    echo "$GIT_PATH" >> .git/info/sparse-checkout
+    git checkout $GIT_BRANCH
+    mv /home/app/webapp/$GIT_PATH/* /home/app/webapp
+fi
+export BROWSER=none
+
+#make sure we're listening
+find . -type f -name "package.json" -exec sed -i 's/webpack-dev-server /webpack-dev-server --host 0.0.0.0 /g' {} +
+
+
+#export NODE_ENV="production"
+npm install -g yarn
+npm install
+npm start  
+
